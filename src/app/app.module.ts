21d9e515fbfbe0b133501fb3021885af620f48ff
RobertoCuevasP <pablorobertocp1@gmail.com>
2021-08-09 17:35:31 -0400

    09/08/2021 Test2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Test2Component
   ],
   imports: [
     BrowserModule
