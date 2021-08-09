1efa596064c548d5f69f1c3f32cf67cfaa269ade
RobertoCuevasP <pablorobertocp1@gmail.com>
2021-08-09 17:55:02 -0400

    09/08/2021 Homework 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Test1Component } from './test1/test1.component';
-
-import { AppComponent } from './app.component';
 import { Test2Component } from './test2/test2.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    Test1Component,
-
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, Test1Component, Test2Component],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
