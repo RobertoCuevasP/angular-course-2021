4e81d4d98dbce0855d4b176ea08c3d8582df4d75
RobertoCuevasP <pablorobertocp1@gmail.com>
2021-08-23 15:45:01 -0400

    18/08/2021 Tasks Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { Directive1Directive } from './directive1.directive';
 import { Directive2Directive } from './directive2.directive';
+import { AdminModule } from './page/admin/admin.module';
+import { BuyModule } from './page/buy/buy.module';
+import { HomeModule } from './page/home/home.module';
 import { Module1Module } from './test1/module1/module1.module';
 import { Test1Component } from './test1/test1.component';
 import { Test2Component } from './test2/test2.component';
@@ -19,7 +22,7 @@ import { Test3Component } from './test3/test3.component';
     Directive1Directive,
     Directive2Directive
   ],
-  imports: [BrowserModule, FormsModule, Module1Module],
+  imports: [BrowserModule, FormsModule, HomeModule, AdminModule, BuyModule],
   providers: [],
   bootstrap: [AppComponent]
 })
