15e7852c035a8522b2a9d5e22bfba79e7949ff96
RobertoCuevasP <pablorobertocp1@gmail.com>
2021-08-30 00:48:34 -0400

    25/08/2021 Navigation by Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,12 +19,26 @@ import { Test1Component } from './test1/test1.component';
 import { Test2Component } from './test2/test2.component';
 import { Test3Component } from './test3/test3.component';
 import { View1Component } from './view1/view1.component';
+import { View1sub1Component } from './view1/view1sub1/view1sub1.component';
+import { View1sub2Component } from './view1/view1sub2/view1sub2.component';
 import { View2Component } from './view2/view2.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'view1', pathMatch: 'full' },
-  { path: 'view1', component: View1Component },
-  { path: 'view2', component: View2Component }
+  {
+    path: 'view1',
+    component: View1Component,
+    children: [
+      { path: '', redirectTo: 'view1sub1', pathMatch: 'full' },
+      { path: 'view1sub1', component: View1sub1Component },
+      { path: 'view1sub2', component: View1sub2Component }
+    ]
+  },
+  { path: 'view2', component: View2Component },
+  {
+    path: 'view3',
+    loadChildren: () => import('./view3/view3.module').then(m => m.View3Module)
+  }
 ];
 
 @NgModule({
@@ -36,6 +50,8 @@ const routes: Routes = [
     Directive1Directive,
     Directive2Directive,
     View1Component,
+    View1sub1Component,
+    View1sub2Component,
     View2Component
   ],
   imports: [
