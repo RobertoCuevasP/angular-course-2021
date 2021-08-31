03f536b47d3a244aeae652bab7ca2938a679fdee
RobertoCuevasP <pablorobertocp1@gmail.com>
2021-08-31 11:22:13 -0400

    27/08/2021 Cleaning Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,7 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
-import { BehaviorSubject, of, Subscription } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  id = 3;
-  data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-  constructor(private router: Router) {
-    this.pure(2, 3);
-    this.impure(2, 3);
-  }
-
-  pure(a: number, b: number) {
-    console.log(a + b);
-    return a + b;
-  }
-
-  impure(a: number, b: number) {
-    const c = Math.random();
-    console.log(a + b + c);
-    return a + b + c;
-  }
-
-  onGoView2FromTs(): void {
-    this.router.navigate(['view2', this.id, 'sub', 17], {
-      queryParams: { name: 'maria', lastname: 'gutierrez' }
-    });
-  }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,108 +1,14 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { AdminComponent } from './ComponentNavigation/admin/admin.component';
-import { Admin1Component } from './ComponentNavigation/admin/admin1/admin1.component';
-import { Admin2Component } from './ComponentNavigation/admin/admin2/admin2.component';
-import { HomeComponent } from './ComponentNavigation/home/home.component';
-import { Home1Component } from './ComponentNavigation/home/home1/home1.component';
-import { Home2Component } from './ComponentNavigation/home/home2/home2.component';
-import { Directive1Directive } from './directive1.directive';
-import { Directive2Directive } from './directive2.directive';
-import { CoreModule } from './HomeworkModules/core/core.module';
-import { LayoutModule } from './HomeworkModules/layout/layout.module';
-import { AdminModule } from './page/admin/admin.module';
-import { BuyModule } from './page/buy/buy.module';
-import { HomeModule } from './page/home/home.module';
-import { ImpurePipe } from './shared/pipes/impure.pipe';
-import { PurePipe } from './shared/pipes/pure.pipe';
-import { SharedModule } from './shared/shared.module';
-import { Module1Module } from './test1/module1/module1.module';
-import { Test1Component } from './test1/test1.component';
-import { Test2Component } from './test2/test2.component';
-import { Test3Component } from './test3/test3.component';
-import { View1Component } from './view1/view1.component';
-import { View1sub1Component } from './view1/view1sub1/view1sub1.component';
-import { View1sub2Component } from './view1/view1sub2/view1sub2.component';
-import { View2Component } from './view2/view2.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'view1', pathMatch: 'full' },
-  {
-    path: 'view1',
-    component: View1Component,
-    children: [
-      { path: '', redirectTo: 'view1sub1', pathMatch: 'full' },
-      { path: 'view1sub1', component: View1sub1Component },
-      { path: 'view1sub2', component: View1sub2Component }
-    ]
-  },
-  { path: 'view2/:id/sub/:id2', component: View2Component },
-  {
-    path: 'view3',
-    loadChildren: () => import('./view3/view3.module').then(m => m.View3Module)
-  },
-  {
-    path: 'admin',
-    component: AdminComponent,
-    children: [
-      { path: 'admin1', component: Admin1Component },
-      { path: 'admin2', component: Admin2Component }
-    ]
-  },
-  {
-    path: 'home',
-    component: HomeComponent,
-    children: [
-      { path: 'home1', component: Home1Component },
-      { path: 'home2', component: Home2Component }
-    ]
-  },
-  {
-    path: 'admin-module',
-    loadChildren: () =>
-      import('./moduleNavigation/admin/admin.module').then(m => m.AdminModule)
-  },
-  {
-    path: 'home-module',
-    loadChildren: () =>
-      import('./moduleNavigation/home/home.module').then(m => m.HomeModule)
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    Test1Component,
-    Test2Component,
-    Test3Component,
-    Directive1Directive,
-    Directive2Directive,
-    View1Component,
-    View1sub1Component,
-    View1sub2Component,
-    View2Component,
-    AdminComponent,
-    Admin1Component,
-    Admin2Component,
-    HomeComponent,
-    Home1Component,
-    Home2Component
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HomeModule,
-    AdminModule,
-    BuyModule,
-    CoreModule,
-    LayoutModule,
-    SharedModule,
-    RouterModule.forRoot(routes)
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
