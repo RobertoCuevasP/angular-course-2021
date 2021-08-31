55a67929fd441696c966710c23a8b65024960367
RobertoCuevasP <pablorobertocp1@gmail.com>
2021-08-31 11:58:44 -0400

    Fixing Errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: `
-    <p>App</p>
-    <router-outlet></router-outlet>
-  `,
-  styleUrls: ['./app.component.scss']
+  <router-outlet></router-outlet>
+`
 })
 export class AppComponent {}
