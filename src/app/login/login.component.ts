000f9e4c4260550a04648d0573473d875d599225
RobertoCuevasP <pablorobertocp1@gmail.com>
2021-09-06 15:26:08 -0400

    03/09/2021 Reactive Form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 import { PublicationService } from './services/publication.service';
 import { SingletonService } from './services/singleton.service';
 import { Test1Service } from './services/test1.service';
@@ -9,15 +15,32 @@ import { Test1Service } from './services/test1.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  name = new FormControl('');
+  formReactive: FormGroup;
+
   constructor(
     private testService: Test1Service,
     private singletonService: SingletonService,
-    private publicationService: PublicationService
+    private publicationService: PublicationService,
+    private formBuilder: FormBuilder
   ) {
     console.log(this.testService);
+    this.formReactive = this.formBuilder.group({
+      name: '',
+      lastName: ['', Validators.required],
+      date: ''
+    });
   }
 
   ngOnInit(): void {
+    this.formReactive.valueChanges.subscribe(res => {
+      console.log('Changes reactive form', res);
+    });
+
+    this.name.valueChanges.subscribe(res => {
+      console.log('Changes', res);
+    });
+
     this.publicationService.getAll().subscribe(s => {
       console.log('Response', s);
     });
@@ -65,5 +88,13 @@ export class LoginComponent implements OnInit {
 
   onSubmitTemplate(values: any) {
     console.log('Values', values);
+  }
+
+  onShow() {
+    console.log(this.name.value);
+  }
+
+  showAll() {
+    console.log('All', this.formReactive.value);
   }
 }
