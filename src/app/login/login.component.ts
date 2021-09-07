1f5c650a1add9c755b87945da2e51c09a97af807
RobertoCuevasP <pablorobertocp1@gmail.com>
2021-09-06 20:31:00 -0400

    06/09/2021 Validations in template form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -15,86 +15,9 @@ import { Test1Service } from './services/test1.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  name = new FormControl('');
-  formReactive: FormGroup;
+  name: string = '';
 
-  constructor(
-    private testService: Test1Service,
-    private singletonService: SingletonService,
-    private publicationService: PublicationService,
-    private formBuilder: FormBuilder
-  ) {
-    console.log(this.testService);
-    this.formReactive = this.formBuilder.group({
-      name: '',
-      lastName: ['', Validators.required],
-      date: ''
-    });
-  }
+  constructor() {}
 
-  ngOnInit(): void {
-    this.formReactive.valueChanges.subscribe(res => {
-      console.log('Changes reactive form', res);
-    });
-
-    this.name.valueChanges.subscribe(res => {
-      console.log('Changes', res);
-    });
-
-    this.publicationService.getAll().subscribe(s => {
-      console.log('Response', s);
-    });
-  }
-
-  onSetMessage(): void {
-    this.singletonService.setMessage('Hi from login');
-  }
-
-  create() {
-    this.publicationService
-      .create({
-        date: '02/09/2021',
-        description: 'I am new',
-        idUser: 126,
-        imageUrl: 'http://firebase/img.jpg',
-        userPhotoUrl: 'http://firebase/user1.jpg'
-      })
-      .subscribe(res => console.log(res));
-  }
-
-  update() {
-    this.publicationService
-      .update('p0001', {
-        date: '02/09/2021',
-        description: 'Hola Mundo Update Testing',
-        idUser: 126,
-        imageUrl: 'http://firebase/img.jpg',
-        userPhotoUrl: 'http://firebase/user1.jpg'
-      })
-      .subscribe(res => console.log(res));
-  }
-
-  patch() {
-    this.publicationService
-      .patch('p0001', {
-        description: 'Hola Mundo Update Testing using Patch'
-      })
-      .subscribe(res => console.log(res));
-  }
-
-  delete() {
-    this.publicationService.delete('p0002').subscribe(res => console.log(res));
-  }
-
-  onSubmitTemplate(values: any) {
-    console.log('Values', values);
-  }
-
-  onShow() {
-    console.log(this.name.value);
-  }
-
-  showAll() {
-    console.log('All', this.formReactive.value);
-  }
+  ngOnInit(): void {}
 }
