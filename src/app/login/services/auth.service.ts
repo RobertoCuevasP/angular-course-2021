576d5dee20f814c12ff9b27350676fb573e0536a
RobertoCuevasP <pablorobertocp1@gmail.com>
2021-09-10 22:55:23 -0400

    09/09/2021 Save token into local storage

--- src/app/login/services/auth.service.ts
+++ src/app/login/services/auth.service.ts
@@ -1,25 +1,33 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../../../environments/environment';
-import { Observable } from 'rxjs';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {environment} from "../../../environments/environment";
+import {map} from "rxjs/operators";
 
 @Injectable()
 export class AuthService {
+
   url = environment.auth.apiBaseUrl;
   key = environment.auth.key;
-  constructor(private http: HttpClient) {}
 
-  public login(body: any): Observable<any> {
-    return this.http.post(
-      `${this.url}/v1/accounts:signInWithPassword?key=${this.key}`,
-      body
+  constructor(private http: HttpClient) { }
+
+  public login(body:any):Observable<any>{
+    return this.http.post(`${this.url}/v1/accounts:signInWithPassword?key=${this.key}`, body).pipe(
+      map( (res:any) => {
+        this.authSuccess(res.idToken, res.localId)
+        return res;
+      })
     );
   }
 
-  public signUp(body: any): Observable<any> {
-    return this.http.post(
-      `${this.url}/v1/accounts:signUp?key=${this.key}`,
-      body
-    );
+  public signUp(body:any){
+    return this.http.post(`${this.url}/v1/accounts:signUp?key=${this.key}`, body);
   }
+
+  private authSuccess(token:string, userId: string){
+    localStorage.setItem('token', token);
+    localStorage.setItem('userId', userId);
+  }
+
 }
