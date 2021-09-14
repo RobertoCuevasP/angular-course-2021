1db9bda3cf358132efa4fa43e2408efeae34dd98
RobertoCuevasP <pablorobertocp1@gmail.com>
2021-09-13 20:28:54 -0400

    Fixing Changes

---
+++ src/app/shared/modules/message-sender/message-sender.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message-sender',
+  templateUrl: './message-sender.component.html'
+})
+export class MessageSenderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
